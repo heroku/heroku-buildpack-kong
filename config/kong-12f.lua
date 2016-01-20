7a7a309ab44c166d54e635c9c8638c3a8cc71ce5
Mars Hall <mars@heroku.com>
2016-01-20 15:54:17 -0800

    Idiomatic Lua: `local` everywhere, underscored names

--- config/kong-12f.lua
+++ config/kong-12f.lua
@@ -1,47 +1,55 @@
-etlua = require "etlua"
+local etlua = require "etlua"
 
 -- 12-factor config generator for Kong
 -- execute with `kong-12f {template-file} {destination-dir}`
 
 -- Reads envirnoment variables for runtime config
-cassandra_url     = os.getenv("CASSANDRA_URL")
-cassandra_cert    = os.getenv("CASSANDRA_TRUSTED_CERT")
-assignedPort      = os.getenv("PORT")
-exposeService     = os.getenv("KONG_EXPOSE") -- `proxy` (default), `admin`, `proxyssl`, `dnsmasq`
+local cassandra_url     = os.getenv("CASSANDRA_URL")
+local cassandra_cert    = os.getenv("CASSANDRA_TRUSTED_CERT")
+local assigned_port     = os.getenv("PORT") or 8000
+local expose_service    = os.getenv("KONG_EXPOSE") -- `proxy` (default), `admin`, `proxyssl`, `dnsmasq`
 
-templateFilename  = arg[1]
-configFilename    = arg[2].."/kong.yml"
-certFilename      = arg[2].."/cassandra.cert"
+local template_filename = arg[1]
+local config_filename   = arg[2].."/kong.yml"
+local cert_filename     = arg[2].."/cassandra.cert"
+
+local proxy_port
+local proxy_ssl_port
+local admin_api_port
+local dnsmasq_port
 
 -- Configure the service to expose on PORT
-if exposeService == "admin" then
+if expose_service == "admin" then
   print("Configuring as Kong admin API")
-  proxy_port = 1 + assignedPort
-  proxy_ssl_port = 2 + assignedPort
-  admin_api_port = assignedPort
-  dnsmasq_port = 3 + assignedPort
-elseif exposeService == "proxyssl" then
+  proxy_port = 1 + assigned_port
+  proxy_ssl_port = 2 + assigned_port
+  admin_api_port = assigned_port
+  dnsmasq_port = 3 + assigned_port
+elseif expose_service == "proxyssl" then
   print("Configuring as Kong SSL proxy")
-  proxy_port = 1 + assignedPort
-  proxy_ssl_port = assignedPort
-  admin_api_port = 2 + assignedPort
-  dnsmasq_port = 3 + assignedPort
-elseif exposeService == "dnsmasq" then
+  proxy_port = 1 + assigned_port
+  proxy_ssl_port = assigned_port
+  admin_api_port = 2 + assigned_port
+  dnsmasq_port = 3 + assigned_port
+elseif expose_service == "dnsmasq" then
   print("Configuring as Kong dnsmasq")
-  proxy_port = 1 + assignedPort
-  proxy_ssl_port = 2 + assignedPort
-  admin_api_port = 3 + assignedPort
-  dnsmasq_port = assignedPort
+  proxy_port = 1 + assigned_port
+  proxy_ssl_port = 2 + assigned_port
+  admin_api_port = 3 + assigned_port
+  dnsmasq_port = assigned_port
 else
   print("Configuring as Kong proxy")
-  proxy_port = assignedPort
-  proxy_ssl_port = 1 + assignedPort
-  admin_api_port = 2 + assignedPort
-  dnsmasq_port = 3 + assignedPort
+  proxy_port = assigned_port
+  proxy_ssl_port = 1 + assigned_port
+  admin_api_port = 2 + assigned_port
+  dnsmasq_port = 3 + assigned_port
 end
 
 -- Expand the comma-delimited list of Cassandra nodes
-cassandra_hosts     = {}
+local cassandra_hosts = {}
+local cassandra_user
+local cassandra_password
+local cassandra_keyspace
 for user, password, host, keyspace in string.gmatch(cassandra_url, "cassandra://([^:]+):([^@]+)@([^/]+)/([^,]+)") do
   cassandra_user      = user
   cassandra_password  = password
@@ -50,11 +58,11 @@ for user, password, host, keyspace in string.gmatch(cassandra_url, "cassandra://
 end
 
 -- Render the Kong configuration file
-templateFile = io.open(templateFilename, "r")
-template = etlua.compile(templateFile:read("*a"))
-templateFile:close()
+local template_file = io.open(template_filename, "r")
+local template = etlua.compile(template_file:read("*a"))
+template_file:close()
 
-config = template({
+local config = template({
   proxy_port          = proxy_port,
   proxy_ssl_port      = proxy_ssl_port,
   admin_api_port      = admin_api_port,
@@ -63,13 +71,14 @@ config = template({
   cassandra_user      = cassandra_user,
   cassandra_password  = cassandra_password,
   cassandra_keyspace  = cassandra_keyspace,
-  cassandra_cert      = certFilename
+  cassandra_cert      = cert_filename
 })
 
-file = io.open(configFilename, "w")
+local file
+file = io.open(config_filename, "w")
 file:write(config)
 file:close()
 
-file = io.open(certFilename, "w")
+file = io.open(cert_filename, "w")
 file:write(cassandra_cert)
 file:close()
