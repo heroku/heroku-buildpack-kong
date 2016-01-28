715628ae6ff0a27cfa3186c31a9d388bee5b3c9a
Mars Hall <mars@heroku.com>
2016-01-28 14:44:15 -0800

    Upgrade to Kong 0.6: use external-supervisor fork; automatic kong-12f config.

--- config/kong-12f.lua
+++ config/kong-12f.lua
@@ -2,19 +2,41 @@ local etlua = require "etlua"
 local lub = require "lub"
 local _ = require "moses"
 
+local S = require "serpent"
+
+local constants = require "kong.constants"
+local config_loader = require "kong.tools.config_loader"
+local services = require "kong.cli.utils.services"
+local cluster_utils = require "kong.tools.cluster"
+local serf_service = require "kong.cli.services.serf"
+
 -- 12-factor config generator for Kong
 -- execute with `kong-12f {template-file} {destination-dir}`
 
 -- Use shell command arguments to set file locations
 -- first arg: the ETLUA template
--- second arg: the config/ directory
+-- second arg: the buildpack/app directory
 local template_filename = arg[1]
-local config_filename   = arg[2].."/kong.yml"
-local cert_filename     = arg[2].."/cassandra.cert"
+local config_filename   = arg[2].."/config/kong.yml"
+local cert_filename     = arg[2].."/config/cassandra.cert"
+
+-- not an `*.sh` file, because the Dyno manager should not exec
+local env_filename  = arg[2].."/.profile.d/kong-env"
 
 -- Read environment variables for runtime config
 local assigned_port     = os.getenv("PORT") or 8000
 local expose_service    = os.getenv("KONG_EXPOSE") -- `proxy` (default), `admin`, `proxyssl`, `dnsmasq`
+
+local cluster_secret    = os.getenv("KONG_CLUSTER_SECRET")
+if not cluster_secret then
+  error("Configuration failed: requires `KONG_CLUSTER_SECRET` environment variable; create with `serf keygen`.")
+end
+local cluster_port      = os.getenv("KONG_CLUSTER_PRIVATE_PORT") or 7946
+local cluster_address   = os.getenv("KONG_CLUSTER_PRIVATE_IP")
+if not cluster_address then
+  error("Configuration failed: requires `KONG_CLUSTER_PRIVATE_IP` environment variable.")
+end
+local cluster_listen    = cluster_address..":"..cluster_port
 
 -- Configure Cassandra using Instaclustr or Heroku-style config vars
 local cassandra_hosts   = {}
@@ -55,6 +77,9 @@ elseif os.getenv("CASSANDRA_URL") ~= nil then
 else
   error("Configuration failed: requires `CASSANDRA_URL` or `IC_CONTACT_POINTS` environment variable.")
 end
+
+-- Prefer replication factor of three or less (then, the number of hosts)
+local cassandra_replication_factor = math.min(3, #cassandra_hosts)
 
 -- Default keyspace to value of `CASSANDRA_KEYSPACE` or simply "kong".
 cassandra_keyspace = cassandra_keyspace or os.getenv("CASSANDRA_KEYSPACE") or "kong"
@@ -110,13 +135,16 @@ local values = {
   proxy_port          = proxy_port,
   proxy_ssl_port      = proxy_ssl_port,
   admin_api_port      = admin_api_port,
+  cluster_listen      = cluster_listen,
+  cluster_secret      = cluster_secret,
   dnsmasq_port        = dnsmasq_port,
   cassandra_hosts     = cassandra_hosts,
   cassandra_user      = cassandra_user,
   cassandra_password  = cassandra_password,
   cassandra_keyspace  = cassandra_keyspace,
   cassandra_ssl       = cassandra_ssl,
-  cassandra_cert      = cert_filename
+  cassandra_cert      = cert_filename,
+  cassandra_replication_factor = cassandra_replication_factor
 }
 
 local config = template(values)
@@ -125,3 +153,36 @@ local config_file
 config_file = io.open(config_filename, "w")
 config_file:write(config)
 config_file:close()
+
+-- Call into kong.cli.services modules to prepare the services (nginx, dnsmasq, serf)
+
+local configuration, configuration_path = config_loader.load_default(config_filename)
+local prepared_services = services.prepare_all(configuration, configuration_path)
+
+-- print("Kong configuration "..S.block(configuration))
+-- print("Kong prepared_services "..S.block(prepared_services))
+
+-- write env vars to `.profile.d` file for Heroku runtime
+-- https://devcenter.heroku.com/articles/profiled
+local env_file
+env_file = io.open(env_filename, "a+")
+
+env_file:write("export KONG_CONF="..prepared_services.nginx._configuration_path.."\n")
+
+env_file:write("export NGINX_WORKING_DIR="..configuration.nginx_working_dir.."\n")
+env_file:write("export NGINX_CONFIG="..constants.CLI.NGINX_CONFIG.."\n")
+
+env_file:write("export DNSMASQ_PORT="..configuration.dns_resolver.port.."\n")
+
+env_file:write("export SERF_CLUSTER_LISTEN="..configuration.cluster_listen.."\n")
+env_file:write("export SERF_CLUSTER_LISTEN_RPC="..configuration.cluster_listen_rpc.."\n")
+env_file:write("export SERF_ENCRYPT="..(configuration.cluster.encrypt or '""').."\n")
+env_file:write("export SERF_NODE_NAME="..cluster_utils.get_node_name(configuration).."\n")
+-- In the event handler, "kong" value is a copy of hardcoded,
+-- local var `EVENT_NAME` in kong.cli.services.serf
+env_file:write("export SERF_EVENT_HANDLER=".."member-join,member-leave,member-failed,member-update,member-reap,user:kong="..prepared_services.serf._script_path.."\n")
+
+-- env_file:seek("set", 0)
+-- print(".profile.d/kong-env.sh: \n"..env_file:read("*a"))
+
+env_file:close()
