cd0e184d0274f71def8bdfe7a7f9b91e0ebb21da
Mars Hall <mars@heroku.com>
2017-08-23 17:09:50 -0700

    Export 12f config to KONG_* env variables; add Admin SSL config

--- config/kong-12f.lua
+++ config/kong-12f.lua
@@ -18,9 +18,10 @@ local config_filename   = arg[2].."/"..rel_config_file
 local env_filename  = arg[2].."/"..rel_env_file
 
 -- Read environment variables for runtime config
-local assigned_port     = os.getenv("PORT") or 8000
-local expose_service    = os.getenv("KONG_EXPOSE") -- `proxy` (default), `admin`, `proxyssl`
-local pg_url            = os.getenv("DATABASE_URL") or "postgres://localhost:5432/kong_dev"
+local address           = "0.0.0.0"
+local port              = os.getenv("PORT") or 8000)
+local expose_service    = os.getenv("KONG_EXPOSE") -- `proxy` (default), `admin`, `adminssl`, `proxyssl`
+local pg_url            = os.getenv("DATABASE_URL") or "postgres://localhost:5432/kong"
 
 local parsed_pg_url = url.parse(pg_url, default)
 
@@ -28,27 +29,37 @@ local pg_host = parsed_pg_url.host
 local pg_port = parsed_pg_url.port
 local pg_user = parsed_pg_url.user
 local pg_password = parsed_pg_url.password
-local pg_dbname = string.sub(parsed_pg_url.path, 2, -1)
+local pg_database = string.sub(parsed_pg_url.path, 2, -1)
 
 -- Configure the service to expose on PORT
-local proxy_port
-local proxy_ssl_port
-local admin_api_port
+local proxy_listen
+local proxy_listen_ssl
+local admin_listen
+local admin_listen_ssl
 if expose_service == "admin" then
   print("Configuring as Kong admin API")
-  proxy_port = 1 + assigned_port
-  proxy_ssl_port = 2 + assigned_port
-  admin_api_port = assigned_port
+  proxy_listen     = address..":"..(port - 2)
+  proxy_listen_ssl = address..":"..(port - 1)
+  admin_listen     = address..":"..(port)
+  admin_listen_ssl = address..":"..(port + 1)
+elseif expose_service == "adminssl" then
+  print("Configuring as Kong admin SSL API")
+  proxy_listen     = address..":"..(port - 3)
+  proxy_listen_ssl = address..":"..(port - 2)
+  admin_listen     = address..":"..(port - 1)
+  admin_listen_ssl = address..":"..(port)
 elseif expose_service == "proxyssl" then
   print("Configuring as Kong SSL proxy")
-  proxy_port = 1 + assigned_port
-  proxy_ssl_port = assigned_port
-  admin_api_port = 2 + assigned_port
+  proxy_listen     = address..":"..(port - 1)
+  proxy_listen_ssl = address..":"..(port)
+  admin_listen     = address..":"..(port + 1)
+  admin_listen_ssl = address..":"..(port + 2)
 else
   print("Configuring as Kong proxy")
-  proxy_port = assigned_port
-  proxy_ssl_port = 1 + assigned_port
-  admin_api_port = 2 + assigned_port
+  proxy_listen     = address..":"..(port)
+  proxy_listen_ssl = address..":"..(port + 1)
+  admin_listen     = address..":"..(port + 2)
+  admin_listen_ssl = address..":"..(port + 3)
 end
 
 -- Render the Kong configuration file
@@ -57,14 +68,15 @@ local template = etlua.compile(template_file:read("*a"))
 template_file:close()
 
 local values = {
-  proxy_port          = proxy_port,
-  proxy_ssl_port      = proxy_ssl_port,
-  admin_api_port      = admin_api_port,
+  proxy_listen          = proxy_listen,
+  proxy_listen_ssl      = proxy_listen_ssl,
+  admin_listen      = admin_listen,
+  admin_listen_ssl  = admin_listen_ssl,
   pg_host             = pg_host,
   pg_port             = pg_port,
   pg_user             = pg_user,
   pg_password         = pg_password,
-  pg_dbname           = pg_dbname
+  pg_database         = pg_database
 }
 
 local config = template(values)
@@ -76,15 +88,25 @@ config_file:close()
 
 print("Wrote Kong config: "..rel_config_file)
 
+-- Also set KONG env vars which **override** config file values.
 -- write env vars to `.profile.d` file for Heroku runtime
 -- https://devcenter.heroku.com/articles/profiled
 local env_file
 env_file = io.open(env_filename, "a+")
 
--- env_file:write("export KONG_CONF=helloworld\n")
+env_file:write("export KONG_PROXY_LISTEN=${KONG_PROXY_LISTEN:-"..proxy_listen.."}\n")
+env_file:write("export KONG_PROXY_LISTEN_SSL=${KONG_PROXY_LISTEN_SSL:-"..proxy_listen_ssl.."}\n")
+env_file:write("export KONG_ADMIN_LISTEN=${KONG_ADMIN_LISTEN:-"..admin_listen.."}\n")
+env_file:write("export KONG_ADMIN_LISTEN_SSL=${KONG_ADMIN_LISTEN_SSL:-"..admin_listen_ssl.."}\n")
 
--- env_file:seek("set", 0)
--- print(".profile.d/kong-env.sh: \n"..env_file:read("*a"))
+env_file:write("export KONG_PG_HOST="..pg_host.."\n")
+env_file:write("export KONG_PG_PORT="..pg_port.."\n")
+env_file:write("export KONG_PG_USER="..pg_user.."\n")
+env_file:write("export KONG_PG_PASSWORD="..pg_password.."\n")
+env_file:write("export KONG_PG_DATABASE="..pg_database.."\n")
+
+env_file:seek("set", 0)
+print(".profile.d/kong-env: \n"..env_file:read("*a"))
 
 env_file:close()
 print("Wrote environment exports: "..rel_env_file)
